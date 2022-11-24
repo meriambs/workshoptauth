f74049daf65dd7a63ca27d55447263acc30205b8
meriambs <bensalah.meriem0@gmail.com>
2022-11-24 18:03:46 +0100

    test

--- config/connect.js
+++ config/connect.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const connectdb=async ()=>{
     try{
+        //ici ne pas oublier a mettre lien de votre atlas 
    await  mongoose.connect('lien a remplacer avec ')
 console.log('you are connected to the db ')
     }catch(err){
